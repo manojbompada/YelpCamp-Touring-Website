e6213af496810a05f6a5374002d6ac51c18c8182
Manoj Bompada <bompadamanoj@gmail.com>
2016-05-28 21:44:18 +0000

    added mongo lab

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ var commentRoutes     = require("./routes/comments"),
     indexRoutes       = require("./routes/index");
  
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+// mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://manojbompada:Bmanoj419@ds019063.mlab.com:19063/yelpcampdb");
 app.use(bodyParser.urlencoded({extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
